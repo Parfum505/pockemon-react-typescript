3e97c0b9c3a64e5e990d5afbe965247afb479995
Pavel Kryvytskyi <pavel.kryvytskyi@gmail.com>
2020-07-22 22:03:45 +0200

    Updates

---
+++ precache-manifest.a18adee0b2a98019b9b8a8f321b2e074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abd4a69461bb500d3823d7b0502358e",
+    "url": "/pockemon-react-typescript/index.html"
+  },
+  {
+    "revision": "37aeffeba523d65ec14a",
+    "url": "/pockemon-react-typescript/static/css/main.71a6f041.chunk.css"
+  },
+  {
+    "revision": "26d9abba8c9d85d65f5c",
+    "url": "/pockemon-react-typescript/static/js/2.3e6733dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pockemon-react-typescript/static/js/2.3e6733dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37aeffeba523d65ec14a",
+    "url": "/pockemon-react-typescript/static/js/main.86ad82a2.chunk.js"
+  },
+  {
+    "revision": "bd47a37b48bc9ff41c8b",
+    "url": "/pockemon-react-typescript/static/js/runtime-main.693f97e0.js"
+  }
+]);
